141740f241d978e5daf4ea9e7c0fc68d09adb48c
TeneoPython01 <TeneoPython01@gmail.com>
2019-11-21 23:37:25 -0600

    LOADING INITIAL CODE
    Starting to put initial structure together to establish a college basketball
    notification process similar to what I've already built for college football.
    This will be a work in progress for a while.  Code is currently functional
    !

---
+++ cbb_data.py
@@ -0,0 +1,23 @@
+# College Basketball Script
+#
+# Author(s):        TeneoPython01@gmail.com
+#
+# Purpose:          This script is used to run the program
+#
+# Special Notes:    n/a
+
+import pandas as pd #to handle data in pandas dataframes
+pd.set_option('display.max_rows', 1000) #allow printing lots of rows to screen
+pd.set_option('display.max_columns', 1000) #allow printsin lots of cols to screen
+pd.set_option('display.width', 1000) #don't wrap lots of columns
+
+import cbbdata as cbb
+import dbutil as dbutil
+
+db = dbutil.Database()
+cbb_data = cbb.cbbData()
+
+#load the watchlist schedules to the database
+db.ingest(cbb_data.watchlist_schedule, 'cbb_schedule_tmp', 'replace')
+
+#print(cbb_data.watchlist_schedule)
